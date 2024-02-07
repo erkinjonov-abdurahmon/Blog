775805c318c090ba8bab120b8ff76cc3be95077a
erkinjonov-abdurahmon <abdurahmonerkinjonov57@gmail.com>
2024-02-07 20:08:06 +0500

    status root added

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -121,6 +121,10 @@ STATICFILES_DIRS = [
     "/var/www/static/",
 ]
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
